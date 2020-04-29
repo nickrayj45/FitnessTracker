31052a96932c21509f83cebc7717c956e5eb244f
Nicholas Jaquez <nrjaquez@gmail.com>
2020-04-28 23:58:27 -0700

    CRUD functions added

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,6 +1,50 @@
 const router = require("express").Router();
-const Workout = require("../models/workout")
+const Workout = require("../models/workout");
 
-router.get("/", (req, res) => {
-    console.log()
-})
+// This is using the router method in order to
+router.post("/api/workout", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+      console.log(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+      console.log(err);
+    });
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(params.id, { $push: { exercises: body } })
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+        console.log(err);
+      });
+  });
+
+  router.delete("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndDelete(params.id, { $push: { exercises: body } })
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+        console.log(err);
+      });
+  });
+
+  module.exports = router;
+
--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
-// const db = mongojs();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// db.on
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
